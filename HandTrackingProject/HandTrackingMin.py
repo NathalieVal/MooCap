278d22204f916e03ef06e62cfb02d052e8790c7d
Nathalie Perez <nat.valetina.perez@gmail.com>
2026-04-28 12:06:23 -0500

    Restarted due to new found information about Legacy Solutions API

--- HandTrackingProject/HandTrackingMin.py
+++ HandTrackingProject/HandTrackingMin.py
@@ -1,14 +1,6 @@
-# Implementing hand tracking code
-# Will follow video w/out pseudocode while setting up tracking
+def main():
 
-import cv2
-import mediapipe as mp
-import time
 
-cap = cv2.VideoCapture(1)
 
-while True:
-    success, img = cap.read()
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+if __name__ == "__main__":
+    main()
