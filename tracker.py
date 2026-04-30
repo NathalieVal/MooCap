574be8822b3d4d605155092ec362ed40b9131ae4
Nathalie Perez <nat.valentina.perez@gmail.com>
2026-04-29 21:56:08 -0500

    Rough pseudocode/ plan for tracking.

---
+++ tracker.py
@@ -0,0 +1,5 @@
+# What do I need for motion capture?
+# 1) Camera input
+# 2) Camera frames into MediaPipe
+# 3) MediaPipe pose/ face/ hand tracking model running at same time (keep it to one for now)
+# 4) Print MediaPipe landmarkers
