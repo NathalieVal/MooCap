ae22d34947d22ef9e482626a4ea3e82332e545b6
Nathalie Perez <nat.valentina.perez@gmail.com>
2026-05-02 11:49:58 -0500

    May 2nd & May 3rd progress plan. Also techinacally pseudocode.

---
+++ PantoneColors.py
@@ -0,0 +1,24 @@
+# - May 3rd:
+#   1. Goal 1: Have main UI prototyped.
+#   2. Goal 2: Color picker is able to get up to three colors from Pantone's selection.
+
+# - May 6th:
+#   1. Goal 1: Create simple visual assets and collect audio required for final product.
+#   2. Goal 2: Begin implementing assets.
+
+# - May 9th:
+#   1. Goal 1: Have final assets implemented.
+#   2. Goal 2: Find bugs or glitches while playing program. TEST RUN. 
+
+# - May 11th
+#   1. Final upload. There are two days between my personal deadline and the actualy deadline. Do NOT rely on this.
+
+
+# MAY 2nd: 
+# Initialize pygame window
+# Potentially figure out custom window sizing if it doesn't take too long just between fullscreen & windowed (https://www.youtube.com/watch?v=edJZOQwrMKw)
+# BASIC windows & menus. Get buttons working.
+
+# MAY 3RD:
+# Figur out color randomizer system ( I have an idea in my head w/ a CSV file)
+
